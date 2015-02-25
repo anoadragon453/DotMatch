0cb44c898dfd1880bc70e4f91b2c1845a70e18e9
Andrew Morgan <anoadragon453@gmail.com>
2015-02-24 19:51:24 -0800

    Forced Text Mode. Changed scale of balls to fit with scaling thingamajig. Fixed up shrinking and moving speed so it works with big objects. (May not work on different sized screens, needs testing).

--- Assets/UserInteraction.cs
+++ Assets/UserInteraction.cs
@@ -3,27 +3,31 @@ using System.Collections;
 
 public class UserInteraction : MonoBehaviour {
 
-	// ------------------- Doubleclick detection ------------------- //
+	// ----------------- Doubleclick detection ---------------- //
 	bool mouseClicksStarted = false;
 	int mouseClicks = 0;
 	float mouseTimerLimit = .2f;
+	bool doubleClickInitiated = false;
 
-	// ------------------- Determine whether this is for user or AI Objects ------------------- //
+	// --- Determine whether this is for user or AI Objects --- //
 	bool isUserObject = true; // Ball will fly to right or left based on this bool
 	bool isMoving = false;
 
-	//
+	// ------------------- Moving Variables ------------------- //
+	float movementVelocity = 3.0f;
 	Vector3 temp;
 
-	//
-	bool doubleClickInitiated = false;
-	float minSize = .1f;
-	float shrinkRate = .7f;
-	float scale = .5f;
+	// ------------------ Shrinking Variables ----------------- //
+	float minSize = 5f;
+	float shrinkRate = 1.0f;
+	float scale = 48.0f;
 	 
 
 	void Start () {
-
+		// Scale object depending on screen size.
+		transform.localScale = new Vector3(Screen.height/16, Screen.height/16,
+0);
+		transform.position = Conversion.makeFixedPoint(transform.position);
 	}
 
 	void OnMouseDown() {
@@ -44,7 +48,7 @@ public class UserInteraction : MonoBehaviour {
 		} else {
 			Debug.Log("Single Clicked");
 			isMoving = true;
-			temp = new Vector3(0.1f,0,0);
+			temp = new Vector3(movementVelocity,0,0);
 			transform.position += temp;
 		}
 		mouseClicksStarted = false;
@@ -63,7 +67,7 @@ public class UserInteraction : MonoBehaviour {
 
 		if (doubleClickInitiated) {
 			transform.localScale = Vector3.one * scale;
-	     	scale -= shrinkRate * Time.deltaTime;
+	     	scale -= shrinkRate * Time.deltaTime * 80.0f; // Since objects are so large in the game space.
 	     	if (scale < minSize) Destroy (gameObject);
 		}
 	}
