1c3e2fd242a2ed89436f15d1966e645a8f005219
Andrew Morgan <anoadragon453@gmail.com>
2015-02-27 12:12:58 -0800

    Created new branch for spawning player dots

---
+++ Assets/PlayerGenerate.cs
@@ -0,0 +1,17 @@
+﻿// Class to spawn random dots for the user to change and send out.
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerGenerate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
