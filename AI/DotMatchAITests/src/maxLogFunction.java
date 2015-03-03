0d7df8a2ad19468a65a5dc5f6b5b1271785a504e
Abhishyant Khare <abhishyant@gmail.com>
2015-03-02 19:36:14 -0800

    ok this will legit commit that java file

---
+++ AI/DotMatchAITests/src/maxLogFunction.java
@@ -0,0 +1,16 @@
+
+public class maxLogFunction {
+
+	public maxLogFunction(){
+		
+	}
+	public double getValue(double time){
+		double logVal = Math.log(Math.log(.1*time));
+		double val = .5*logVal + .6831;
+		return val;
+	}
+	public void printMax(){
+		int difficulty = 25;
+		
+	}
+}
---
+++ AI/DotMatchAITests/src/runTime.java
@@ -0,0 +1,9 @@
+
+public class runTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
