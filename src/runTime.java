099385debdcaae07e5b45e6c35213e5fb4ab5172
Abhishyant Khare <abhishyant@gmail.com>
2015-03-02 20:48:15 -0800

    AI DONE

--- src/runTime.java
+++ src/runTime.java
@@ -3,6 +3,9 @@ public class runTime {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		maxLogVal test = new maxLogVal();
+		System.out.println("Starting...");
+		test.printMax();
 
 	}
 
